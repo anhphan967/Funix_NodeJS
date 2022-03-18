686719a3902339516b0a6eff631eaff7b61f7f13
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-18 16:00:06 +0700

    lab1.4

--- first-app.js
+++ first-app.js
@@ -3,7 +3,19 @@ fs.writeFileSync('hello.txt', 'Hello from node.js');
 
 // create node_server
 const http = require('http')
+
+
 const server = http.createServer((req, res) => {
+        //change request
+    const url=req.url 
+    if(req.url==='/'){    
+    res.write('<html>')
+    res.write('<head><title>My first page </title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>submitd</button></input></form></body>')
+    res.write('</html>')    
+    return res.end()
+    }
+
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>My first page </title></head>')
