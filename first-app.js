9a5e1d9e9e8d9b8dc3f75589c09f3c6e4a17ad72
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-18 11:02:46 +0700

    lab1.3

--- first-app.js
+++ first-app.js
@@ -1,9 +1,14 @@
-const fs=require('fs');
+const fs = require('fs');
 fs.writeFileSync('hello.txt', 'Hello from node.js');
 
 // create node_server
- const http= require('http')  
- const server = http.createServer((req,res)=>{
-     console.log(req)
- })
- server.listen(3000)
+const http = require('http')
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My first page </title></head>')
+    res.write('<body><h1>Hello from my nodejs server</h1></body>')
+    res.write('</html>')
+    res.end()
+})
+server.listen(3000)
