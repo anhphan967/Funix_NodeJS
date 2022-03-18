b436bc8d165da04c7c674d391c609b88dd0beacd
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-18 10:45:58 +0700

    lab1.2

--- first-app.js
+++ first-app.js
@@ -1,2 +1,9 @@
 const fs=require('fs');
 fs.writeFileSync('hello.txt', 'Hello from node.js');
+
+// create node_server
+ const http= require('http')  
+ const server = http.createServer((req,res)=>{
+     console.log(req)
+ })
+ server.listen(3000)
