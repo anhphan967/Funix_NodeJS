e92362076bf3e50c8b3e5bd8d72497715dd8d2d4
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-21 09:20:22 +0700

    lan2.2

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http')
+//install express
 const express = require('express')
 const app= express()
-
+// midleware 
+app.use((res, rep, next)=>{
+   console.log('in first midleware 1')
+   next() // allow the request to continue the next midleware in line
+})
+app.use((res, rep, next)=>{
+   console.log('in first midleware 2')
+   //
+})
 const server = http.createServer(app)
 server.listen(3000)
