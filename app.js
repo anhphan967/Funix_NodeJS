2649c0ac20dbb161da4b8ed53adb7bda0796783e
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-21 11:29:08 +0700

    lab22.7

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const http = require('http')
+const adminRouter= require('./routers/admin')
+const shopRouter=require('./routers/shop')
 //install express
 const express = require('express')
 const app= express()
 //body post
 app.use(express.urlencoded())
-// midleware 
-app.use((req, res, next)=>{
-   console.log('allways run')
-   next() // allow the request to continue the next midleware in line
-})
-app.use('/add-product',(req, res, next)=>{
-   res.send('<form action="/product" method="POST"><input type="text" name="tittle"/><button>Submit</button></form>')   
-   next()
-   
-})
-app.post('/product',(req, res, next)=>{
-   console.log(req.body)
-   res.redirect('/')
-   }) 
-app.use('/',(req, res, next)=>{
-   res.send('<h1>Hello from Express.js</h1>')   
-   next()
-})  
+
+app.use(adminRouter)
+app.use(shopRouter)
+
+
+ 
 const server = http.createServer(app)
 server.listen(3000)
