5ef7815a8e71e3e57c63671d897b31d548fc7d3d
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-21 10:12:34 +0700

    lab2.4

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const express = require('express')
 const app= express()
 // midleware 
 app.use((res, rep, next)=>{
-   console.log('in first midleware 1')
+   console.log('allways run')
    next() // allow the request to continue the next midleware in line
 })
-app.use((res, rep, next)=>{
-   rep.send('<h1>hello</h1>')
-   console.log('in first midleware 2')
+app.use('/add-product',(res, rep, next)=>{
+   rep.send('<h1>The Add product page</h1>')   
+   next()
+   //
+})
+app.use('/',(res, rep, next)=>{
+   rep.send('<h1>Hello from Express.js</h1>')   
    //
 })
 const server = http.createServer(app)
