78dea87b291db1e498bb6e98db1759e73444cf0d
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-21 16:52:31 +0700

    lab2.12

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const adminRouter= require('./routers/admin')
 const shopRouter=require('./routers/shop')
+const path= require('path')
 //install express
 const express = require('express')
 const app= express()
@@ -10,7 +11,7 @@ app.use(express.urlencoded())
 app.use('/admin',adminRouter)
 app.use(shopRouter)
 app.use((req,res,next)=>{
-   res.status(404). send('<h1>Page not found </h1>')
+   res.status(404). sendFile(path.join(__dirname,'views','404.html'))
 })
  
 const server = http.createServer(app)
