e20027102f5222a113351a80f290b6cfe40c7726
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-25 15:41:18 +0700

    lab4.4

--- model/product.js
+++ model/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs= require('fs')
+const path= require('path');
 
 module.exports = class Product {
     constructor(t) {
@@ -6,10 +7,27 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const linkData= path.join(path.dirname(require.main.filename),'data','products.json')
+        fs.readFile(linkData,(err, fileContent)=>{
+            let products = []; 
+            if(!err){
+                products=JSON.parse(fileContent)
+            }
+            products.push(this);
+            fs.writeFile(linkData, JSON.stringify(products), err =>{
+                console.log(err)
+            })
+        })        
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const linkData= path.join(path.dirname(require.main.filename),'data','products.json')
+             
+            fs.readFile(linkData, (err, fileContent) => {
+                if (err) {
+                  return []
+                }
+                return (JSON.parse(fileContent));
+              });      
     }
 }
