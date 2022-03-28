5529ad5f2dd16c64307f2a83487e6e101617dbbb
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-28 15:22:23 +0700

    chinh sưa duu liêu sp

--- models/product.js
+++ models/product.js
@@ -19,6 +19,7 @@ const getProductsFromFile = cb => {
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
+    this.id=id
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,11 +27,24 @@ module.exports = class Product {
   }
   save() {
     this.id=Math.random().toString();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+    getProductsFromFile( ( products ) =>
+    {
+      if ( this.id )
+      {
+        const existingProductIndex = products.findIndex( prod => prod.id === this.id )
+        const updatedProducts = [ ...products ]
+        updatedProducts[ existingProductIndex ] = this
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
         console.log(err);
       });
+      } else
+      {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });        
+      }
     });
   }
 
