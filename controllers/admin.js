01fd892d900444f3c3ef8ddd350ea866c64aafa6
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-28 15:55:33 +0700

    Xoa cac muc trong gio hang

--- controllers/admin.js
+++ controllers/admin.js
@@ -40,6 +40,8 @@ exports.getEditProduct = (req,res,next)=>{
 exports.postDeleteProduct = ( req, res, next ) =>
 {
   const prodId = req.body.productId
+  Product.deleteById( prodId )
+  res.redirect('/admin/products')
 } 
 
 exports.postEditProduct = (req, res, next) => {
