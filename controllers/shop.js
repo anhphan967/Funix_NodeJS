488f0c401a24ee8db837730581d2f1424b0c524b
Phan Anh Tuan <tuancdt067@gmail.com>
2022-03-31 16:56:27 +0700

    tim nap san pham

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,16 @@ const Product = require('../models/product');
 const Cart= require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      console.log(rows);
+      res.render("shop/index", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProduct=(req,res, next)=>{
