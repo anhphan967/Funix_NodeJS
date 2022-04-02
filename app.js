5ee7d0515b63c85495750a6d7b727588cd4d51a7
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-02 22:11:15 +0700

    Tao va quan ly nguoi dung tam thoi

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 
+const Product = require("./models/product");
+const User = require("./models/user");
+
+
 const errorController = require("./controllers/error");
 
 const app = express();
@@ -12,18 +16,33 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const req = require("express/lib/request");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
+
+Product.belongsTo(User, { constraint: true, onDelete: "CASCADE" });
+User.hasMany(Product);
 
 sequelize
   .sync()
-  .then((res) => {
+  .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "tuan", email: "tuan123@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
