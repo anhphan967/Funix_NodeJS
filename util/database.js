033d9c99e6caa135f44dc596e5ad91fbafe4ed54
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-02 15:10:40 +0700

    ket noi voi csdl

--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "nodejs",
-  password: "batho123",
-});
+const sequelize = new Sequelize("nodejs", "root", "batho123", {
+  dialect: "mysql",
+  host: "localhost"});
 
-module.exports = pool.promise();
+module.exports =sequelize;
