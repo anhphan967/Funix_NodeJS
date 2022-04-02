070532adc80003eb68016e4766eb76d1b66f7f0e
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-02 16:46:14 +0700

    xoa sp

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProduct = (req, res, next) => {
   })
     .then(() => {
       console.log("Created");
+      res.redirect("/admin/products");
     })
     .catch((err) => console.log(err));
 };
@@ -81,6 +82,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+  .then((product) => {    
+    return product.destroy();
+  })
+  .then((result) => {
+    console.log("DESTROY PRODUCT");
+    res.redirect("/admin/products");
+  })
+  .catch((err) => console.log(err));
 };
