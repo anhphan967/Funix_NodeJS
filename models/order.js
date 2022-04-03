5cadfc3b8116388a7c1296205d3c6191d8582727
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-03 15:42:37 +0700

    tao them 1 model order

--- models/order.js
+++ models/order.js
@@ -2,14 +2,13 @@ const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
 
-const OrderItem = sequelize.define("orderItem", {
+const Order = sequelize.define("order", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  quantity: Sequelize.INTEGER,
 });
 
-module.exports = OrderItem;
+module.exports = Order;
