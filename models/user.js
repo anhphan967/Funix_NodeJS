dd2a4a8f34863bfb62a5b585058d4f46c6416795
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-08 22:56:51 +0700

    lam viec tren cac gio hang và don hang trong gio

--- models/user.js
+++ models/user.js
@@ -2,15 +2,27 @@ const mongodb = require("mongodb");
 const getDb= require("../util/database").getDb
 const ObjectId =mongodb.ObjectId
 class User{
-  constructor(name, email){
+  constructor(name, email, id){
     this.name= name;
     this.email= email;
+    this._id=id
   }
   save(){
     const db= getDb();
     return db.collection('user').insertOne(this)
 
   }
+
+  addCart(product){
+    const updatedCart={ item: {...product, quantity:1}}
+    const db=getDb()
+    return db
+          .collection("users")
+          .updateOne(
+            { _id: new mongodb.ObjectId(this._id=id)},
+            {$set:{cart:item}})
+  }
+
   static findById(userId){
     const db= getDb();
     return db
