7071c5d66678100a0c64037b3b32f551754a4ac6
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-09 17:35:06 +0700

    them 1 don hang

--- controllers/shop.js
+++ controllers/shop.js
@@ -113,30 +113,9 @@ exports.postCart = (req, res, next) => {
   //   .catch((err) => console.log(err));
 };
 
-exports.postOrder = (req, res, next) => {
-  let fetchedCart;
+exports.postOrder = (req, res, next) => {  
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProduct(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then((result) => {
-      return fetchedCart.setProducts(null);
-    })
+    .addOrder()   
     .then((result) => {
       res.redirect("/orders");
     })
