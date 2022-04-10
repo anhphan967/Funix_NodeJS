2521e10ebbb818a05a264577171819a4f7702639
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-10 11:38:49 +0700

    Them va su dung model nguoi dung

--- app.js
+++ app.js
@@ -13,27 +13,38 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("62504de920cdf5f8b8faad69")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("62525d677157157ec1c8e9e9")
+    .then((user) => {      
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-// app.use(errorController.get404);
+app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://nodejs:batho123@cluster0.ipyxs.mongodb.net/shop?retryWrites=true&w=majority')
-  .then(result=>{    
-    app.listen(3000)
-  })
+.then((result) => {
+  User.findOne().then((user) => {
+    if (!user) {
+      const user = new User({
+        name: "tuan",
+        email: "tuan@test.com",
+        cart: [],
+      });
+      user.save();
+    }
+  });
+  app.listen(3000);
+})
   .catch(err=>{
     console.log(err)
   })
