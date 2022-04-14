d014dce920ae9ac16f43c5fe07b755344ac862e3
Phan Anh Tuan <tuancdt067@gmail.com>
2022-04-14 16:52:25 +0700

    xoa session

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,17 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {    
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false,
+    isAuthenticated:req.session.isLoggedIn,
   });
 };
 exports.postLogin = (req, res, next) => {
   //res.setHeader("Set-Cookie", "loggedIn=true, HttpOnly");
   req.session.isLoggedIn=true;
   res.redirect('/login');
+};
+exports.outLogin = (req, res, next) => {
+  req.session.destroy((err)=>{
+    console.log(err),
+    res.redirect('/')
+  })  
 };
