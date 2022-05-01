be26bf46d72da9ecded1f0087a98f4457c5392c6
Phan Anh Tuan <tuancdt067@gmail.com>
2022-05-01 11:29:23 +0700

    loc tap tin bang minetype

--- app.js
+++ app.js
@@ -20,7 +20,23 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+
 const csrfProtection = csrf();
+const fileStorage=multer.diskStorage({
+  destination: (req, file, cb)=>{
+     cb(null, 'images');
+  },
+  filename:(req, file, cb)=>{
+    cb(null, new Date().toISOString() +'-'+ file.originalname)
+  }
+})
+const fileFilter= (req,file, cb)=>{
+  if(file.mimetype =='image/jpeg'||file.mimetype =='image/png'||file.mimetype =='image/jpg'){
+    cb(null,true)
+  }else{
+    cb(null,false)
+  }
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -30,7 +46,8 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({dest:'images'}).single('image'))
+app.use(multer({storage: fileStorage, fileFilter:fileFilter}).single('image'))
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
@@ -69,7 +86,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.use('/500',errorController.get500)
+
 app.use(errorController.get404);
 app.use((error,req, res, next)=>{
   //res.statuses(error.httpStatusCode).render(...)
--- controllers/error.js
+++ controllers/error.js
@@ -3,8 +3,4 @@ exports.get404 = (req, res, next) => {
   path: '/404',  
 });
 };
-exports.get500 = (req, res, next) => {
-  res.status(500).render('500', { pageTitle: 'Erros', 
-  path: '/500',  
-});
-};
+
